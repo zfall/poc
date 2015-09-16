79beb3460cbdfa13188f3124b199c9b250c4e165
Zoltan Fall <fall.zoltan@gmail.com>
2015-09-16 22:29:36 +0200

    Changing test code to test files

--- spring-poc/src/test/java/hu/fall/poc/spring/integration/BaseIntegrationTest.java
+++ spring-poc/src/test/java/hu/fall/poc/spring/integration/BaseIntegrationTest.java
@@ -8,20 +8,35 @@ import javax.jms.Session;
 import javax.jms.TextMessage;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.testng.annotations.Test;
 
-public class ServiceActivatorTest {
-	public static void main(String[] args) throws Exception{
-		String config = "spring/integration/service-activator.xml";
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(config);
+public abstract class BaseIntegrationTest {
+
+	protected ClassPathXmlApplicationContext context;
+
+	@Test
+	void defaultTest() throws Exception{
+		context = new ClassPathXmlApplicationContext(getConfigFile());
 		context.start();
 
+		testContent();
+
+		Thread.sleep(10000);
+		context.close();
+	}
+
+	protected abstract String getConfigFile();
+
+	protected abstract void testContent() throws Exception;
+
+	protected void sendTextMessage(String queueName, String message) throws Exception {
 		ConnectionFactory connectionFactory = (ConnectionFactory)context.getBean("connectionFactory");
 		Connection connection = connectionFactory.createConnection();
 		try {
 			Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 			try {
-				TextMessage textMessage = session.createTextMessage("any kind of text");
-				Queue helloWorldDestination = session.createQueue("helloWorldDestination");
+				TextMessage textMessage = session.createTextMessage(message);
+				Queue helloWorldDestination = session.createQueue(queueName);
 				MessageProducer messageProducer = session.createProducer(helloWorldDestination);
 				messageProducer.send(textMessage);
 			} finally {
@@ -30,8 +45,5 @@ public class ServiceActivatorTest {
 		} finally {
 			connection.close();
 		}
-
-		Thread.sleep(10000);
-		context.close();
 	}
 }
---
+++ spring-poc/src/test/java/hu/fall/poc/spring/integration/InboundHelloWorldServiceActivatorTest.java
@@ -0,0 +1,18 @@
+package hu.fall.poc.spring.integration;
+
+import org.testng.annotations.Test;
+
+@Test
+public class InboundHelloWorldServiceActivatorTest extends BaseIntegrationTest {
+
+	@Override
+	protected String getConfigFile() {
+		return "spring/integration/service-activator.xml";
+	}
+
+	@Override
+	protected void testContent() throws Exception
+	{
+		sendTextMessage("helloWorldDestination", "any kind of text");
+	}
+}
